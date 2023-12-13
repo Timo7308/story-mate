3455003ef615991409b2ed67fccdb3e16e9f0170
Lars <lars.hurrelbrink@gmail.com>
2023-12-13 16:38:29 +0100

    changed theme swatch to our primary color.

--- story_mate/lib/main.dart
+++ story_mate/lib/main.dart
@@ -20,20 +20,43 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+//function that creates a swatch based on our primary color. Derived from: https://medium.com/@nickysong/creating-a-custom-color-swatch-in-flutter-554bcdcb27f3
+  MaterialColor createMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    Map<int, Color> swatch = {};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    for (var strength in strengths) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    }
+    ;
+
+    return MaterialColor(color.value, swatch);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final primaryColor = Color(0xFF0A2342);
     return MaterialApp(
       home: const WelcomePage(),
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primaryColor: Color(0xFF0A2342), // Set the primary color
-
+        primaryColor: primaryColor,
         colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.green), // This also sets the primary color
+          primarySwatch: createMaterialColor(primaryColor),
+        ),
         useMaterial3: true,
-        scaffoldBackgroundColor: Colors.white,
+        scaffoldBackgroundColor: Colors.white, //white background
       ),
     );
   }
