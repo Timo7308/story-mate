e1fcd0f8469b7134b0d827ffb92c955a6de81310
timo5 <timo5@uni-bremen.de>
2023-12-14 10:44:37 +0100

    Move finish setup button down

--- story_mate/lib/profile/profile.dart
+++ story_mate/lib/profile/profile.dart
@@ -4,7 +4,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'setup.dart';
+import 'setup.dart'; // Ensure this is your correct import
 
 enum Gender { male, female, other }
 
@@ -26,76 +26,30 @@ class _ProfilePageState extends State<ProfilePage> {
       appBar: AppBar(
         title: const Text('Setup your profile'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(height: 20),
-            Center(
-              child: _profileImage(),
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
-                onPressed: () => _pickAndUploadImage(),
-                child: const Text('Upload Profile Picture'),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  const SizedBox(height: 20),
+                  Center(child: _profileImage()),
+                  const SizedBox(height: 20),
+                  _uploadButton(),
+                  _genderChoiceTile(Gender.male, 'Male'),
+                  _genderChoiceTile(Gender.female, 'Female'),
+                  _genderChoiceTile(Gender.other, 'Other'),
+                  const SizedBox(height: 20),
+                ],
               ),
             ),
-            ListTile(
-              title: const Text('Male'),
-              leading: Radio<Gender>(
-                value: Gender.male,
-                groupValue: _selectedGender,
-                onChanged: (Gender? value) {
-                  setState(() {
-                    _selectedGender = value;
-                  });
-                  _saveGenderToFirebase('male');
-                },
-              ),
-            ),
-            ListTile(
-              title: const Text('Female'),
-              leading: Radio<Gender>(
-                value: Gender.female,
-                groupValue: _selectedGender,
-                onChanged: (Gender? value) {
-                  setState(() {
-                    _selectedGender = value;
-                  });
-                  _saveGenderToFirebase('female');
-                },
-              ),
-            ),
-            ListTile(
-              title: const Text('Other'),
-              leading: Radio<Gender>(
-                value: Gender.other,
-                groupValue: _selectedGender,
-                onChanged: (Gender? value) {
-                  setState(() {
-                    _selectedGender = value;
-                  });
-                  _saveGenderToFirebase('other');
-                },
-              ),
-            ),
-            const SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SetupPage()),
-            );
-          },
-          child: Text('Finish Setup'),
-        ),
-          ],
-        ),
+          ),
+          _finishSetupButton(),
+        ],
       ),
     );
   }
-
   Widget _profileImage() {
     if (_profileImageUrl != null) {
       return Image.network(
@@ -108,6 +62,61 @@ class _ProfilePageState extends State<ProfilePage> {
       return const Icon(Icons.account_circle, size: 150);
     }
   }
+
+  Widget _uploadButton() {
+    return Center(
+      child: SizedBox(
+        width: 300, // Set the width as per your requirement
+        child: ElevatedButton(
+          onPressed: () => _pickAndUploadImage(),
+          style: ElevatedButton.styleFrom(
+            // If you want to adjust the padding inside the button
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          ),
+          child: const Text('Upload Profile Picture'),
+        ),
+      ),
+    );
+  }
+  Widget _genderChoiceTile(Gender gender, String text) {
+    return ListTile(
+      title: Text(text),
+      leading: Radio<Gender>(
+        value: gender,
+        groupValue: _selectedGender,
+        onChanged: (Gender? value) {
+          setState(() {
+            _selectedGender = value;
+          });
+          _saveGenderToFirebase(text.toLowerCase());
+        },
+      ),
+    );
+  }
+
+  Positioned _finishSetupButton() {
+    return Positioned(
+      bottom: 10, // Reduced value to move the button higher
+      left: 15,
+      right: 15,
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.9, // Adjust width as needed
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 20), // Add padding to move the button up
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SetupPage()),
+              );
+            },
+            child: const Text('Finish Setup'),
+          ),
+        ),
+      ),
+    );
+  }
+
 
   Future<void> _pickAndUploadImage() async {
     final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
