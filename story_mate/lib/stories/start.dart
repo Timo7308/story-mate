016966717e36cecebf376767f627ca6e241a4ff4
timo5 <timo5@uni-bremen.de>
2023-12-14 15:49:25 +0100

    Created a page where the AI stuff should happen

--- story_mate/lib/stories/start.dart
+++ story_mate/lib/stories/start.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'story_chat.dart';
 
 class StartPage extends StatelessWidget {
   @override
@@ -10,6 +11,17 @@ class StartPage extends StatelessWidget {
       body: const Center(
         child: Text('Random connection or choose story'),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigate to another page when the button is pressed
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => StoryChatPage()), // Replace 'NextPage' with your destination page class
+          );
+        },
+        child: const Icon(Icons.navigate_next), // Icon for the button
+      ),
     );
   }
 }
+
---
+++ story_mate/lib/stories/story_chat.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+// Example of a destination page class
+class StoryChatPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Next Page'),
+      ),
+      body: Center(
+        child: Text('Welcome to the next page!'),
+      ),
+    );
+  }
+}
