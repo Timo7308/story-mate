6891e62cf239aeba7427279296b94827ca45e84c
Amin Aghazadeh <amin_aghazadeh23@yahoo.com>
2024-01-27 16:32:40 +0100

    Big changes: AI Messages repositioning, Chat Ending functionality (1), Initial AI Message as a Beginning

--- story_mate/lib/stories/story_chat.dart
+++ story_mate/lib/stories/story_chat.dart
@@ -5,6 +5,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:story_mate/profile/profile_chat_partner.dart';
+import 'package:story_mate/stories/story_finished_chat.dart';
 
 class StoryChatPage extends StatefulWidget {
   final String loggedInUserId;
@@ -27,11 +28,78 @@ class _StoryChatPageState extends State<StoryChatPage> {
   final List<types.Message> messages = [];
   final TextEditingController _textController = TextEditingController();
 
+  String tellAStoryMessage = '';
+  String aiBeginningText = '';
+
   @override
   void initState() {
     super.initState();
     _listenForMessages();
+    _tellAStoryOnPageLoad(); // call the method when the page loads
   }
+
+  // // new method to handle "Tell a story" on page load
+  // void _tellAStoryOnPageLoad() async {
+
+  //   tellAStoryMessage = 'What is a Pirate?';
+  //   // if(widget.storyTitle == "A Pirate Tale"){
+  //   //   tellAStoryMessage = 'What is a Pirate?';
+  //   // }
+  //   // if(widget.storyTitle == "A Zombie Apocalypse"){
+  //   //   tellAStoryMessage = 'What is a Zombie?';
+  //   // }
+  //   await _getBeginningResponse(tellAStoryMessage); // empty dynamicPrompt
+  // }
+
+
+  void _tellAStoryOnPageLoad() async {
+
+    // Pirate tale
+    if (widget.storyTitle == "A Pirate Tale"){
+      tellAStoryMessage = 'What is a Pirate?';
+    }
+    // Space Adventure
+    else if(widget.storyTitle == "A Space Adventure"){
+      tellAStoryMessage = 'What is a Space?';
+    }
+    // Medieval Story
+    else if(widget.storyTitle == "A Medieval Story"){
+      tellAStoryMessage = 'What is a Medieval?';
+    }
+    // Fairy Tale
+    else if(widget.storyTitle == "A Fairy Tale"){
+      tellAStoryMessage = 'What is a Fairy?';
+    }
+    // Zombie Apocalypse
+    else if(widget.storyTitle == "A Zombie Apocalypse"){
+      tellAStoryMessage = 'What is a Zombie?';
+    }
+    aiBeginningText = await _getBeginningResponse(tellAStoryMessage);
+  }
+
+
+  // // new button widget for "Tell a story"
+  // IconButton _buildTellAStoryButton() {
+  //   return IconButton(
+  //     icon: const Icon(Icons.book),
+  //     onPressed: _tellAStoryPressed,
+  //   );
+  // }
+
+  void _finishChat() {
+    // Close the current chat screen
+    Navigator.of(context).pop();
+
+    // Navigate to the "StoryFinishedChatScreen" defined in story_finished_chat.dart
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => StoryFinishedChatScreen(),
+      ),
+    );
+  }
+
+
+
 
   void _listenForMessages() {
     DatabaseReference chatRef = FirebaseDatabase.instance.ref('chats/${widget.chatId}');
@@ -71,40 +139,75 @@ class _StoryChatPageState extends State<StoryChatPage> {
     // Add your logic for dynamicPrompt here
 
     String dynamicPrompt = '';
+
     // dynamicPrompt should be changed based on the selected story
 
     //final dynamicPrompt = 'Convert this message into a short part (50 words) of the a story which is "Pirate" themed. The purpose of this story is knowing each other. Do not repeat what the I say. Do not mention any proper names in the content. Put the content of the message in the story and If there are any questions, put them into the story in a question format too: ' + userText;
 
     // Pirate tale
-     if (widget.storyTitle == "A Pirate Tale"){
-       dynamicPrompt = 'Convert this message into a short part (50 words) of the a story which is "Pirate tale" themed. The purpose of this story is knowing each other. Do not repeat what the I say. Do not mention any proper names in the content. Put the content of the message in the story and If there are any questions, put them into the story in a question format too: ' + userText;
-       //dynamicPromptForBeginning = '';
-     }
+    if (widget.storyTitle == "A Pirate Tale"){
+      dynamicPrompt = 'Convert this message into a short part (50 words) of the a story which is "Pirate tale" themed. The purpose of this story is knowing each other. Do not repeat what the I say. Do not mention any proper names in the content. Put the content of the message in the story and If there are any questions, put them into the story in a question format too: ' + userText;
+      //dynamicPromptForBeginning = 'What is the result of 1 plus 3?';
+    }
     // Space Adventure
-     else if(widget.storyTitle == "A Space Adventure"){
-       dynamicPrompt = 'Convert this message into a short part (50 words) of the a story which is "Space Adventure" themed. The purpose of this story is knowing each other. Do not repeat what the I say. Do not mention any proper names in the content. Put the content of the message in the story and If there are any questions, put them into the story in a question format too: ' + userText;
-       //dynamicPromptForBeginning = '';
-     }
+    else if(widget.storyTitle == "A Space Adventure"){
+      dynamicPrompt = 'Convert this message into a short part (50 words) of the a story which is "Space Adventure" themed. The purpose of this story is knowing each other. Do not repeat what the I say. Do not mention any proper names in the content. Put the content of the message in the story and If there are any questions, put them into the story in a question format too: ' + userText;
+      //dynamicPromptForBeginning = '';
+    }
     // Medieval Story
-     else if(widget.storyTitle == "A Medieval Story"){
-       dynamicPrompt = 'Convert this message into a short part (50 words) of the a story which is "Medieval Story" themed. The purpose of this story is knowing each other. Do not repeat what the I say. Do not mention any proper names in the content. Put the content of the message in the story and If there are any questions, put them into the story in a question format too: ' + userText;
-       //dynamicPromptForBeginning = '';
-     }
+    else if(widget.storyTitle == "A Medieval Story"){
+      dynamicPrompt = 'Convert this message into a short part (50 words) of the a story which is "Medieval Story" themed. The purpose of this story is knowing each other. Do not repeat what the I say. Do not mention any proper names in the content. Put the content of the message in the story and If there are any questions, put them into the story in a question format too: ' + userText;
+      //dynamicPromptForBeginning = '';
+    }
     // Fairy Tale
-     else if(widget.storyTitle == "A Fairy Tale"){
-       dynamicPrompt = 'Convert this message into a short part (50 words) of the a story which is "Fairy Tale" themed. The purpose of this story is knowing each other. Do not repeat what the I say. Do not mention any proper names in the content. Put the content of the message in the story and If there are any questions, put them into the story in a question format too: ' + userText;
-       //dynamicPromptForBeginning = '';
-     }
+    else if(widget.storyTitle == "A Fairy Tale"){
+      dynamicPrompt = 'Convert this message into a short part (50 words) of the a story which is "Fairy Tale" themed. The purpose of this story is knowing each other. Do not repeat what the I say. Do not mention any proper names in the content. Put the content of the message in the story and If there are any questions, put them into the story in a question format too: ' + userText;
+      //dynamicPromptForBeginning = '';
+    }
     // Zombie Apocalypse
-     else if(widget.storyTitle == "A Zombie Apocalypse"){
-       dynamicPrompt = 'Convert this message into a short part (50 words) of the a story which is "Zombie Apocalypse" themed. The purpose of this story is knowing each other. Do not repeat what the I say. Do not mention any proper names in the content. Put the content of the message in the story and If there are any questions, put them into the story in a question format too: ' + userText;
-       //dynamicPromptForBeginning = '';
-     }
+    else if(widget.storyTitle == "A Zombie Apocalypse"){
+      dynamicPrompt = 'Convert this message into a short part (50 words) of the a story which is "Zombie Apocalypse" themed. The purpose of this story is knowing each other. Do not repeat what the I say. Do not mention any proper names in the content. Put the content of the message in the story and If there are any questions, put them into the story in a question format too: ' + userText;
+      //dynamicPromptForBeginning = '';
+    }
     print('Dynamic Prompt: $dynamicPrompt');
 
     _sendMessage(userText, widget.loggedInUserId, widget.chatId);
     await _getResponse(userText,dynamicPrompt);
   }
+
+
+
+
+
+  Future<String> _getBeginningResponse(String tellAStoryMessage) async {
+    final apiKey = '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'; // Replace with your actual API key
+    final endpoint = 'https://api.openai.com/v1/chat/completions';
+
+    // Construct the messages list based on the selected story
+    final messages = [
+      {'role': 'user', 'content': tellAStoryMessage},
+    ];
+
+
+    final response = await http.post(
+      Uri.parse(endpoint),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $apiKey',
+      },
+      body: jsonEncode({
+        'model': 'gpt-3.5-turbo-1106',
+        'messages': messages,
+        'max_tokens': 150,
+      }),
+    );
+    if (true) {
+      final data = jsonDecode(response.body);
+      final aiBeginningText = data['choices'][0]['message']['content'].trim();
+      return aiBeginningText;
+    }
+  }
+
   Future<void> _getResponse(String userText, String dynamicPrompt) async {
     final apiKey = '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'; // Replace with your actual API key
     final endpoint = 'https://api.openai.com/v1/chat/completions';
@@ -132,7 +235,8 @@ class _StoryChatPageState extends State<StoryChatPage> {
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
       final aiText = data['choices'][0]['message']['content'].trim();
-      _sendMessage(aiText, 'ai-id', widget.chatId); // Send AI response
+
+      _sendMessage(aiText, widget.loggedInUserId, widget.chatId); // Send AI response
     } else {
       // Handle error...
     }
@@ -146,14 +250,47 @@ class _StoryChatPageState extends State<StoryChatPage> {
     );
   }
 
+  // AppBar _buildAppBar() {
+  //   print('Story Title: ${widget.storyTitle}');
+  //   return AppBar(
+  //     leading: _buildBackButton(),
+  //     title: Text(widget.storyTitle),
+  //     actions: [
+  //       _buildProfileButton(),
+  //     ],
+  //   );
+  // }
+
   AppBar _buildAppBar() {
     print('Story Title: ${widget.storyTitle}');
     return AppBar(
-        automaticallyImplyLeading: false,
       title: Text(widget.storyTitle),
-      actions: [_buildProfileButton()],
+      actions: [
+        _buildFinishChatButton(),
+        _buildProfileButton(),
+      ],
     );
   }
+
+  IconButton _buildFinishChatButton() {
+    return IconButton(
+      icon: const Icon(Icons.check),
+      onPressed: () {
+        _showFinishChatDialog();
+      },
+    );
+  }
+
+
+
+  // IconButton _buildBackButton() {
+  //   return IconButton(
+  //     icon: const Icon(Icons.arrow_back_ios_new_outlined),
+  //     onPressed: () {
+  //       Navigator.maybePop(context);
+  //     },
+  //   );
+  // }
 
   IconButton _buildProfileButton() {
     return IconButton(
@@ -171,10 +308,66 @@ class _StoryChatPageState extends State<StoryChatPage> {
     );
   }
 
+
+
+
+  void _showFinishChatDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Leaving the story'),
+          content: Text('Are you sure you want to leave the story?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Implement logic to finish the chat and navigate back
+                Navigator.of(context).pop(); // Close the dialog
+                _finishChat();
+              },
+              child: Text('Yes'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+// void _finishChat() {
+
+//   // Close the current chat screen
+//   Navigator.of(context).pop();
+
+//   // Navigate back to the "story_choice.dart" screen
+//   Navigator.pushReplacementNamed(context, '/story_choice.dart');
+// }
+
+
+
+
+
+
+
+
+
+
 // new chat body scheme including beginning of the stories
+
   Column _buildChatBody() {
     return Column(
       children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: StoryTextSection(text: aiBeginningText),
+          ),
+        ),
         Expanded(
           child: Chat(
             messages: messages,
@@ -192,10 +385,43 @@ class _StoryChatPageState extends State<StoryChatPage> {
             },
           ),
         ),
-       // StoryTextSection(text: 'Beginning of the Stories'),
       ],
     );
   }
+
+
+
+// Column _buildChatBody() {
+//   return Column(
+//     children: [
+//       Align(
+//         alignment: Alignment.topLeft,
+//         child: Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: StoryTextSection(text: aiBeginningText ?? ""),
+//         ),
+//       ),
+//       Expanded(
+//         child: Chat(
+//           messages: messages,
+//           user: types.User(id: widget.loggedInUserId),
+//           theme: _buildChatTheme(),
+//           showUserNames: true,
+//           customBottomWidget: _buildInputField(),
+//           onSendPressed: _handleSendPressed,
+//           customMessageBuilder: (message, {required int messageWidth}) {
+//             if (message is types.TextMessage) {
+//               return _buildCustomTextMessage(
+//                   message as types.TextMessage, messageWidth);
+//             }
+//             return _buildDefaultMessageContainer(message, messageWidth);
+//           },
+//         ),
+//       ),
+//     ],
+//   );
+// }
+
 
   // Column _buildChatBody() {
   //   return Column(
@@ -312,13 +538,13 @@ class StoryTextSection extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(8.0),
       margin: const EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        color: Colors.green, // You can customize the color
-        borderRadius: BorderRadius.circular(8.0),
-      ),
+      // decoration: BoxDecoration(
+      //   color: Colors.green, // You can customize the color
+      //   borderRadius: BorderRadius.circular(8.0),
+      // ),
       child: Text(
         text,
-        style: TextStyle(color: Colors.white),
+        style: TextStyle(color: Color(0xFF0A2342)),
       ),
     );
   }
---
+++ story_mate/lib/stories/story_finished_chat.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:story_mate/profile/profile_chat_partner.dart';
+import 'package:story_mate/stories/story_finished_chat.dart';
+import 'package:flutter/material.dart';
+import 'package:story_mate/stories/story_choice.dart';  // Import the StoryChoiceScreen or replace it with the actual destination page
+
+class StoryFinishedChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Story Ended'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'You left the story.',
+              style: TextStyle(fontSize: 20),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);  // Pop the current screen (StoryFinishedChatScreen)
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ChoicePage()),
+                );  // Push the StoryChoiceScreen
+              },
+              child: Text('Back to Story Choice page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
